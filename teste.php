018c63cc0988781fb0082663ae94d6a5159dedc5
Yure <yurecadastros@gmail.com>
2026-01-14 10:41:32 -0300

    add: Instruções para João

--- teste.php
+++ teste.php
@@ -1,1 +1,7 @@
 teste
+João vou aproveitar esse seu arquivo para colocar algumas instruções para treino.
+Como falamos pelo Teams tente implementar nesse Projeto o uso do Padrão I18n para traduções.
+Pode usar apoio do ChatGPT para isso ele te ajuda a imlementar sem problemas.
+Começe com um padrão simples, com os dados armazenados em arquivo e depois evolua para um conceito de tradução que possua interface para alimentar os dados de tradução.
+
+Abraço Yurê
